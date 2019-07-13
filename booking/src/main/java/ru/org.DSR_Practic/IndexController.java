577139c4b2bcdd0553d02ea380dff20f1ebecac3
Anatoliy Udarczev <toly.udarczev@yandex.ru>
2019-07-13 15:07:56 +0300

    Hello world!

---
+++ booking/src/main/java/ru/org.DSR_Practic/IndexController.java
@@ -0,0 +1,16 @@
+package ru.org.DSR_Practic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String getIndex(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
